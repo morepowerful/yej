4fb0847845417e2b63d92a96fd4d482fc2fabfb8
liudong <liu.dong@ycofoundation.com>
2017-08-15 14:00:55 +0800

    1.0.0

---
+++ bin/yej.js
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+'use strict';
+var process = require('process')
+var commander = require('commander')
+var chalk = require('chalk')
+var child_process = require('child_process')
+var unzip = require('unzip')
+var download = require('download')
+var fs = require('fs')
+var inquirer = require('inquirer')
+
+commander.version('1.0.0')
+commander.on('--help', function() {
+  console.log('******************   ' + chalk.yellow('API:') + '   ******************');
+  console.log('');
+  console.log(chalk.yellow('                  yej -h,yej --help：帮助信息'));
+  console.log(chalk.yellow('                  yej -v,yej --version：显示yej脚手架版本'));
+  console.log(chalk.yellow('                  yej init：初始化一个react+redux项目'));
+  console.log(chalk.yellow('                  yej install：安装项目的依赖'));
+  console.log(chalk.yellow('                  yej server：开启项目服务'));
+  console.log(chalk.yellow('                  yej build：打包项目'));
+})
+
+commander.parse(process.argv);
+if (!process.argv[2]) {
+  var help = child_process.exec('node index.js --help', function(err, stdout, stdree) {
+    if (err) {
+      throw err;
+    }
+    process.stdout.write(stdout);
+  })
+  return;
+}
+
+switch (process.argv[2]) {
+  case "init":
+    initFunction();
+    break;
+  case "install":
+    installFunction();
+    break;
+  case "server":
+    serverFunction();
+    break;
+  case "build":
+    buildFunction();
+    break;
+  default:
+  console.log(chalk.yellow('  Can\'t find \'node index.js '+ process.argv[2] +'\',Please ensure the right commends'));
+}
+
+function initFunction() {
+  if(process.argv[3]) {
+    if(process.argv[3] == '-h' || process.argv[3] == '--help') {
+      console.log(chalk.yellow("        帮你初始化一个react+redux+webpack项目"));
+    } else{
+      console.log(chalk.red('Please ensure the right commends'));
+    }
+  } else{
+    try{
+      download('https://github.com/morepowerful/yej-template/archive/master.zip', process.cwd()).then(() => {
+        var unzipFile = unzip.Extract({path:process.cwd()})
+        fs.createReadStream('yej-template-master.zip').pipe(unzipFile)
+        unzipFile.on('close', function() {
+          fs.unlinkSync('yej-template-master.zip')
+        })
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+}
+
+function installFunction() {
+  if(process.argv[3]) {
+    if(process.argv[3] == '-h' || process.argv[3] == '--help') {
+      console.log(chalk.yellow("        yej install  帮你安装项目所需的依赖包"));
+    } else{
+      console.log(chalk.red('Please ensure the right commends'));
+    }
+  } else{
+    if(process.cwd().indexOf('yej-template-master') > 0) {
+      inquirer.prompt({
+        type: 'list',
+        name: 'installPath',
+        choices : ['npm','cnpm'],
+        default: 'npm',
+        message: '请选择你要用什么进行安装（Please choose the resource that you want to install）'
+      }).then(anwser => {
+        try{
+          var install
+          if(anwser.installPath == 'cnpm') {
+            install = child_process.execSync('cnpm install')
+          } else {
+            install = child_process.execSync('npm install')
+          }
+          process.stdout.write(install)
+        } catch(err){
+          console.log(err);
+        }
+      })
+    } else {
+      console.log(chalk.yellow('请进入'+process.cwd()+'/yej-template-master文件里进行安装'));
+    }
+  }
+}
+
+function serverFunction() {
+  if(process.argv[3]) {
+    if(process.argv[3] == '-h' || process.argv[3] == '--help') {
+      console.log(chalk.yellow("        yej server开启项目服务"));
+    } else{
+      console.log(chalk.red('Please ensure the right commends'));
+    }
+  } else{
+    try{
+      var server = child_process.execSync('npm run start')
+      process.stdout.write(server);
+    }catch(err) {
+      console.log(err);
+    }
+  }
+}
+
+function buildFunction() {
+  if(process.argv[3]) {
+    if(process.argv[3] == '-h' || process.argv[3] == '--help') {
+      console.log(chalk.yellow("        yej build命令来打包项目"));
+    } else{
+      console.log(chalk.red('Please ensure the right commends'));
+    }
+  } else {
+    try {
+      var build = child_process('npm run build')
+      process.stdout.write(build)
+    }catch(err) {
+      console.log(err);
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+## Usage
+
+- Generate a boilerplate.
+
+```bash
+$ yej init
+```
+
+- Install the dependencies.
+
+```bash
+$ cd xx(the project path)
+$ yej install
+```
+
+- Start a local dev server.
+
+```bash
+$ yej server
+```
+> It will listen to the port 8080
+
+- Build project.
+
+```bash
+$ yej build
+```
